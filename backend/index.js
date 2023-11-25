b7b5ba0c556342dc47a2944453adcb02c9aaf606
Akshay-IDC <76376872+Akshay-IDC@users.noreply.github.com>
2023-11-25 22:09:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const port = 5000 || process.env.port
 const  mongoDB=require('./database');
 mongoDB();
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin","https://go-foodie.netlify.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-width, Content-Type, Accept"
