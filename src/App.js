3a7f7eac121d84a17a3efad080f5271baf65e407
Akshay-IDC <76376872+Akshay-IDC@users.noreply.github.com>
2023-11-25 22:20:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
   Route,
   
 } from "react-router-dom";
-//import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 // eslint-disable-next-line
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
